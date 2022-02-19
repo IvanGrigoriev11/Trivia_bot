0fadcbb967a4933fa1abd5dc0ecb94a1b28306a0
Andrei <none>
2022-02-19 07:26:57 -0800

    removed config

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
- 
-env_var = input('Please enter the environment variable name:\n')
- 
-if env_var in os.environ:
-    print(f'{env_var} value is {os.environ[env_var]}')
-else:
-    print(f'{env_var} does not exist')
