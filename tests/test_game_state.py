95602b5c5bae46e60d0a5642dd802df9e5eea109
Ivan Grigoriev <11ivangrigoriev@gmail.com>
2022-04-12 04:28:24 -0700

    simplified check function

--- tests/test_game_state.py
+++ tests/test_game_state.py
@@ -1,5 +1,4 @@
-from http import client
-from bot_state import GameState, IdleState
+from bot_state import GameState
 from telegram_client import TelegramClient, Update, SendMessagePayload, Message, Chat
 from typing import List
 from models import Question
@@ -16,26 +15,16 @@ class FakeTelegramClient(TelegramClient):
         self.sent_messages.append(payload)
 
 
-def check(user_message: str, expected_messages: List[str]):
+def check(last_question: bool, user_message: str, expected_messages: List[str]):
     client = FakeTelegramClient()
-    questions = [
-        Question("question 1", ["a", "b", "c"], 0),
-        Question("question 2", ["a", "b", "c"], 1)
-    ]
-    state = GameState(client, questions)
-    chat_id = 111
-    state.on_enter(chat_id)
-    state.process(Update(123, Message(Chat(chat_id), user_message)))
-
-    expected_payloads = [SendMessagePayload(chat_id, text) for text in expected_messages]
-
-    assert client.sent_messages == expected_payloads
-
-
-def check22(user_message: str, expected_messages: List[str]):
-    client = FakeTelegramClient()
-    questions = [
-        Question("question 3", ["a", "b", "c"], 2),
+    if last_question != True:
+        questions = [
+            Question("question 1", ["a", "b", "c"], 0),
+            Question("question 2", ["a", "b", "c"], 1)
+        ]
+    else: 
+        questions = [
+        Question("question 3", ["a", "b", "c"], 2)
     ]
     state = GameState(client, questions)
     chat_id = 111
@@ -48,7 +37,7 @@ def check22(user_message: str, expected_messages: List[str]):
 
 
 def test_end_game_score_1_of_1():
-    check22("2", [
+    check(True, "2", [
         "question 3\n['a', 'b', 'c']",
         "You are right",
         "You got 1 points out of 1.\nIf you want to try again, type /startGame to start a new game."
@@ -56,7 +45,7 @@ def test_end_game_score_1_of_1():
 
 
 def test_end_game_score_0_of_1():
-    check22("1", [
+    check(True, "1", [
         "question 3\n['a', 'b', 'c']",
         "You are wrong",
         "You got 0 points out of 1.\nIf you want to try again, type /startGame to start a new game."
@@ -64,7 +53,7 @@ def test_end_game_score_0_of_1():
 
 
 def test_right_asnwer():
-    check("0", [
+    check(False, "0", [
         "question 1\n['a', 'b', 'c']",
         "You are right",
         "question 2\n['a', 'b', 'c']"
@@ -72,7 +61,7 @@ def test_right_asnwer():
 
 
 def test_wrong_answer():
-    check("1", [
+    check(False, "1", [
         "question 1\n['a', 'b', 'c']",
         "You are wrong",
         "question 2\n['a', 'b', 'c']"
@@ -80,7 +69,7 @@ def test_wrong_answer():
 
 
 def test_others_from_client():
-    check("others", [
+    check(False, "others", [
         "question 1\n['a', 'b', 'c']",
         "please, type the number of your supposed answer",
         "question 1\n['a', 'b', 'c']"
