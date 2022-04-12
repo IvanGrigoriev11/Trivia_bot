89f3fd7c1ced5ae27c6ec61500c38267461b02d4
Ivan Grigoriev <11ivangrigoriev@gmail.com>
2022-04-12 03:06:16 -0700

    added all possible scenarios

--- tests/test_game_state.py
+++ tests/test_game_state.py
@@ -1,4 +1,5 @@
-from bot_state import GameState
+from http import client
+from bot_state import GameState, IdleState
 from telegram_client import TelegramClient, Update, SendMessagePayload, Message, Chat
 from typing import List
 from models import Question
@@ -29,6 +30,37 @@ def check(user_message: str, expected_messages: List[str]):
     expected_payloads = [SendMessagePayload(chat_id, text) for text in expected_messages]
 
     assert client.sent_messages == expected_payloads
+
+
+def check22(user_message: str, expected_messages: List[str]):
+    client = FakeTelegramClient()
+    questions = [
+        Question("question 3", ["a", "b", "c"], 2),
+    ]
+    state = GameState(client, questions)
+    chat_id = 111
+    state.on_enter(chat_id)
+    state.process(Update(123, Message(Chat(chat_id), user_message)))
+
+    expected_payloads = [SendMessagePayload(chat_id, text) for text in expected_messages]
+
+    assert client.sent_messages == expected_payloads
+
+
+def test_end_game_score_1_of_1():
+    check22("2", [
+        "question 3\n['a', 'b', 'c']",
+        "You are right",
+        "You got 1 points out of 1.\nIf you want to try again, type /startGame to start a new game."
+    ])
+
+
+def test_end_game_score_0_of_1():
+    check22("1", [
+        "question 3\n['a', 'b', 'c']",
+        "You are wrong",
+        "You got 0 points out of 1.\nIf you want to try again, type /startGame to start a new game."
+    ])
 
 
 def test_right_asnwer():
