464d9cf4efc32293623265bb73f7715ca59513c6
Ivan Grigoriev <11ivangrigoriev@gmail.com>
2022-05-06 03:08:43 -0700

    v1.3

--- src/main.py
+++ src/main.py
@@ -16,10 +16,7 @@ def main():
     while True:
         for update in client.get_updates(offset):
             offset = update.update_id + 1
-            if update.message is not None:
-                chat_id = update.message.chat.id
-            else:
-                chat_id = update.callback_query.from_.id
+            chat_id = update.chat_id
 
             if chat_id not in chat_handlers:
                 chat_handlers[chat_id] = ChatHandler.make_default(client, chat_id)
