562a7f2da55812683ad479d5df7074fb9f39b355
Ivan Grigoriev <11ivangrigoriev@gmail.com>
2022-05-12 00:21:38 -0700

    fixed one comment

--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,6 @@ def parse_int(s: str) -> Optional[int]:
 
 
 def transform_keywords(key: str) -> str:
-    """Appends "_" to keys which are Python-reserved keyword. E.g. `from` -> `from_`."""
+    """Appends "_" to keys which are Python-reserved keywords. E.g. `from` -> `from_`."""
 
     return key if key != "from" else "from_"
