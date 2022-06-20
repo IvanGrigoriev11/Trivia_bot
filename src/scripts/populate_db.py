03cb5ebaf58a7f8ea55eda1dd7c9ef993b71aa62
Ivan Grigoriev <11ivangrigoriev@gmail.com>
2022-06-20 11:25:59 -0700

    rewritten script

--- src/scripts/populate_db.py
+++ src/scripts/populate_db.py
@@ -1,13 +1,22 @@
 import json
 import os
+from dataclasses import dataclass
 
+import html2text
 import psycopg
 import typer
 
-DB_NAME = "postgres"
+
+@dataclass()
+class Common:
+    dbname: str
+    host: str
+    port: int
+    file: str
 
 
-def main(
+def connect(
+    ctx: typer.Context,
     dbname: str = typer.Argument(
         ..., help="The name of the database which you are calling"
     ),
@@ -19,78 +28,111 @@ def main(
         5432,
         help="Type the number of port. 5432 is set by default.",
     ),
+    file: str = typer.Option(
+        "questions.json",
+        help="Type the name of the file which you want to populate to the database.",
+    ),
 ):
+    """Connects to the selected database."""
+
+    ctx.obj = Common(dbname, host, port, file)
+
+    user = os.environ["TRIVIA_POSTGRES_USER"]
+    password = os.environ["TRIVIA_POSTGRES_PASSWD"]
+
+    # pylint: disable = not-context-manager
+    with psycopg.connect(
+        host=host, dbname=dbname, user=user, password=password, port=port
+    ) as conn:
+
+        print("Connection is set up")
+        create(conn, ctx.obj.file)
+    print("Connection was closed")
+
+
+def create(conn, file):
     """Creates 'questions' and 'answers' tables in the selected database."""
 
-    if dbname == DB_NAME:
-        user = os.environ["USER_FOR_DATABASE"]
-        password = os.environ["PASSWORD_FOR_DB"]
-
-        # pylint: disable = not-context-manager
-        with psycopg.connect(
-            host=host, dbname=dbname, user=user, password=password, port=port
-        ) as conn:
-
-            print("Connection is set up")
-            # Open a cursor to perform database operations
-            with conn.cursor() as cur:
-                # Execute a command: this creates a new table
-                cur.execute(
-                    """
-                    CREATE TABLE IF NOT EXISTS questions (
-                        id serial PRIMARY KEY,
-                        category text,
-                        type text,
-                        difficulty text,
-                        question text
-                        )
-                    """
+    # Open a cursor to perform database operations
+    with conn.cursor() as cur:
+        # Execute a command: this creates a new table
+        cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS questions (
+                id integer PRIMARY KEY,
+                category text,
+                type text,
+                difficulty text,
+                question text
                 )
+            """
+        )
 
-                cur.execute(
-                    """
-                    CREATE TABLE IF NOT EXISTS answers (
-                        question_id serial PRIMARY KEY,
-                        correct_answer text,
-                        incorrect_answers text
-                        )
-                    """
+        cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS answers (
+                question_id integer,
+                text text,
+                is_correct bool,
+                FOREIGN KEY (question_id) 
+                    REFERENCES questions(id) 
                 )
-                with open("questions.json", mode="r", encoding="utf-8") as my_file:
-                    all_questions = json.loads(my_file.read())
+            """
+        )
+        with open(file, encoding="utf-8") as f:
+            all_questions = json.load(f)
 
-                for index, question in enumerate(all_questions):
-                    cur.execute(
-                        "INSERT INTO questions (id, category, type, difficulty, question)"
-                        "VALUES (%s, %s, %s, %s, %s)",
-                        (
-                            index + 1,
-                            f"{question['category']}",
-                            f"{question['type']}",
-                            f"{question['difficulty']}",
-                            f"{question['question']}",
-                        ),
-                    )
-                    cur.execute(
-                        "INSERT INTO answers (question_id, correct_answer, incorrect_answers)"
-                        "VALUES (%s, %s, %s)",
-                        (
-                            index + 1,
-                            f"{question['correct_answer']}",
-                            f"{question['incorrect_answers']}",
-                        ),
-                    )
-                print("'questions' and 'answers' tables were created")
+        for index, question in enumerate(all_questions):
+            question["question"] = html2text.html2text(
+                f"{question['question']}"
+            ).strip()
+            cur.execute(
+                "INSERT INTO questions (id, category, type, difficulty, question)"
+                "VALUES (%s, %s, %s, %s, %s) RETURNING id;",
+                (
+                    index + 1,
+                    f"{question['category']}",
+                    f"{question['type']}",
+                    f"{question['difficulty']}",
+                    f"{question['question']}",
+                ),
+            )
+            question_id = cur.fetchone()[0]
+            for answer in range(len(question["incorrect_answers"])):
                 cur.execute(
-                    """
-                    SELECT COUNT (*) FROM questions;
-                    """
+                    "INSERT INTO answers (question_id, text, is_correct)"
+                    "VALUES (%s, %s, %s)",
+                    (
+                        question_id,
+                        f"{question['incorrect_answers'][answer]}",
+                        False,
+                    ),
                 )
-                print(f"Questions in the database: {cur.fetchone()}")
-            print("Connection was closed")
-    else:
-        print("Database not found")
+            cur.execute(
+                "INSERT INTO answers (question_id, text, is_correct)"
+                "VALUES (%s, %s, %s)",
+                (
+                    question_id,
+                    question["correct_answer"],
+                    True,
+                ),
+            )
+
+        print("'questions' and 'answers' tables were created")
+        count_rows_in_table(cur, "questions")
+        count_rows_in_table(cur, "answers")
+
+
+def count_rows_in_table(cur, table_name: str):
+    """Counts rows in the selected table."""
+
+    cur.execute(
+        f"""
+        SELECT COUNT (*) FROM {table_name};
+        """
+    )
+    print(f"{table_name} in the database: {cur.fetchone()[0]}")
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    typer.run(connect)
