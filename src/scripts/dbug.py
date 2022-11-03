c85105c18bc985ab54ef9c6b204d0ee08d228145
Ivan <11ivangrigoriev@gmail.com>
2022-11-03 21:47:16 +0600

    222

--- src/scripts/dbug.py
+++ src/scripts/dbug.py
@@ -1,8 +1,23 @@
+import typer
 from fastapi import FastAPI
+from uvicorn import Config, Server
 
-app = FastAPI()
+
+def main(
+    host: str = typer.Option("localhost", help="server host"),
+    port: int = typer.Option(8000, help="server port"),
+):
+    app = FastAPI()
+
+    @app.get("/")
+    async def run_server_mode():
+        return {"message": "Hello World"}
+
+    conf = Config(app=app, host=host, port=port, debug=True)
+
+    server = Server(conf)
+    server.run()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+if __name__ == "__main__":
+    typer.run(main)
