a515f0e6b586766da1b886079310854d49945269
Ivan <11ivangrigoriev@gmail.com>
2022-11-23 00:14:55 +0600

    kjhhh

--- src/telegram_client.py
+++ src/telegram_client.py
@@ -223,7 +223,6 @@ class LiveTelegramClient(TelegramClient):
 
         if response.status_code != 200:
             raise UnexpectedStatusCodeException(response.status_code, response.reason)
-        return None
 
     def get_updates(self, offset: int = 0) -> List[Update]:
         return self._request(
